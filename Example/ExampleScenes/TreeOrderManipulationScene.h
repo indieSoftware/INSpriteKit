088f5f8ff7946e4617930b30a08d5df77d82ab52
Sven Korset <Sven.Korset@indie-Software.de>
2014-07-08 13:09:13 +0200

    Added a new test scene for the node tree manipulation methods

---
+++ Example/ExampleScenes/TreeOrderManipulationScene.h
@@ -0,0 +1,13 @@
+//
+//  TreeOrderManipulationScene.h
+//  INSpriteKitExample
+//
+//  Created by Sven Korset on 08.07.14.
+//  Copyright (c) 2014 indie-Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TreeOrderManipulationScene : NSObject
+
+@end
---
+++ Example/ExampleScenes/TreeOrderManipulationScene.m
@@ -0,0 +1,13 @@
+//
+//  TreeOrderManipulationScene.m
+//  INSpriteKitExample
+//
+//  Created by Sven Korset on 08.07.14.
+//  Copyright (c) 2014 indie-Software. All rights reserved.
+//
+
+#import "TreeOrderManipulationScene.h"
+
+@implementation TreeOrderManipulationScene
+
+@end
